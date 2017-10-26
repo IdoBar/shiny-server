66d0ae4e62b5ef15f1edd9c2e43db68615928a20
Ido Bar <ibar@usc.edu.au>
2017-10-26 16:42:21 +0000

    Updated documentation and usage of util.R

--- README.md
+++ README.md
@@ -3,13 +3,19 @@ A shiny server app to deliver processed and annotated transcriptome data stored 
 
 ## Main features
 * Accessible from a web browser (from anywhere)
-* Provide basic summarized information (annotation, expression)
+* Provide basic summarized information (sequences, annotation, expression)
 * Provide basic plotting (expression) 
 
 ## Implementation
 ### Hosting
 * Shiny server hosted on a cloud-service (AWS, NECTAR, DigitalOcean, etc.) - see [details](http://deanattali.com/2015/05/09/setup-rstudio-shiny-server-digital-ocean/)
 * Mount USC's main servers using `sshfs` - see [details](https://www.digitalocean.com/community/tutorials/how-to-use-sshfs-to-mount-remote-file-systems-over-ssh) 
+
+### Transcript information
+* Select from available transcriptomes  
+* Allow filtering based on keyword/transcript/gene  
+* Download selected sequences or annotation table
+
 ### Expression
-* Search by keyword, return all BLAST matches (in a table), show their expression (in a graph)
+* Show selected transcript expression (in a table and a graph)
 * Allow consolidating multiple transcripts under one gene name/symbol 
--- shinotate/app.R
+++ shinotate/app.R
@@ -6,7 +6,8 @@
 #
 #    http://shiny.rstudio.com/
 #
-CRAN_packages <- c("shiny", "data.table", "tidyverse", "sqldf", "dbplyr", "DT")
+devtools::source_gist("7f63547158ecdbacf31b54a58af0d1cc", filename = "util.R")
+CRAN_packages <- c("shiny",  "tidyverse", "sqldf", "dbplyr", "DT")
 install.deps(CRAN_packages)
 
 # Define UI for application that draws a histogram
@@ -27,11 +28,11 @@ ui <- fluidPage(
       # Show a results table of transcripts matching the search
       mainPanel(
         
-        h2("Pearl Oyster (", em("P. maxima"), ") Trinotate transcriptome database"), 
+        h2("Pearl Oyster (", em("P. maxima"), ") Mantle transcriptome database"), 
         p("Mantle tissues from pearl oysters were collected, RNA was extracted and was subjected to RNA-Sequencing."),
         p("The resulting data was ", em("de novo"), " assembled into a reference transcriptome, annotated and stored in a ",
           a("Trinotate", href="http://trinotate.github.io/") ," database"),
-        p("Use the search bar on the left panel to retrieve transcripts whose BLAST annotation matches the keyword."),
+        p("Use the search bar on the left panel to retrieve transcripts whose BLAST annotation matches the keyword. The the search boxes in the table can then be used to further apply filters. The table can then be exported using the ", strong("Download"), "button."),
         br(), br(),
         DT::dataTableOutput("output_table")
       )
@@ -40,7 +41,7 @@ ui <- fluidPage(
 
 # Define server logic required to show a results table of transcripts matching the search
 server <- function(input, output) {
-  mantle_db <- src_sqlite("/mnt/bioinfo03/data/Paspaley/Mantle/Trinity_assembly/Annotation/Trinotate_db/P_maxima_mantle_Trinotate_local.sqlite")
+  mantle_db <- src_sqlite("/srv/shiny-server/data/Paspaley/Mantle/Trinity_assembly/Annotation/Trinotate_db/P_maxima_mantle_Trinotate_local.sqlite")
   # retrieve blast and pfam results (remove P_maxima_Mantle... to match the accessions in the databases)
   
   mantle_annot <- tbl(mantle_db, sql("SELECT TrinityId, Description, BitScore, Pfam.HMMERDomains, Pfam.DomainDescriptions, H.GO_ids, H.GO_names, K.KO_ID AS KEGG_id, K.KO_name AS KEGG_name  FROM Blast_tax B LEFT OUTER JOIN (SELECT QueryProtID,GROUP_CONCAT(HMMERDomain, ';')  HMMERDomains ,GROUP_CONCAT(HMMERTDomainDescription, ';')  DomainDescriptions FROM HMMERDbase WHERE FullSeqScore>20 AND FullDomainScore>20 GROUP BY QueryProtID) Pfam ON B.TrinityId=Pfam.QueryProtID LEFT OUTER JOIN (SELECT * FROM HMMERDbase JOIN (SELECT pfam_acc, group_concat(name) AS GO_names, group_concat(go_id) AS GO_IDs FROM (SELECT * FROM pfam2go JOIN go ON pfam2go.go_id=go.id) GROUP BY pfam_acc) G ON substr(HMMERDbase.pfam_id,1,7)=G.pfam_acc WHERE FullSeqScore>20 AND FullDomainScore>20 GROUP BY QueryProtID HAVING MAX(FullSeqScore) AND MAX(FullDomainScore) ) H ON B.TrinityId=H.QueryProtID LEFT OUTER JOIN (SELECT * FROM ORF_KO WHERE KO_id != 'None') K ON B.TrinityId=K.orf_id GROUP BY TrinityId HAVING MAX(BitScore)")) %>% 
